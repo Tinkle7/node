e49b2aaea55200cd7af6c23b70495e8b871e2d28
tinklef22 <tinkle@f22labs.com>
2023-12-27 16:44:20 +0530

    how to create server and listen

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import http from "http";
 import express from "express";
+
+const PORT = process.env.SERVER_PORT;
 
 require("dotenv").config();
 
@@ -15,10 +16,4 @@ app.use((req, res, next) => {
   res.send("<h1>Hello from express</h1>");
 });
 
-const server = http.createServer(app);
-
-const PORT = process.env.SERVER_PORT;
-
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+app.listen(PORT);
