e96df0e6296639061e7e6fda8b64514bc96e4d35
tinklef22 <tinkle@f22labs.com>
2023-12-27 16:16:50 +0530

    basic of middleware

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,15 @@ require("dotenv").config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("in the middleware_1");
+  next(); // this allows to continue to the next function
+});
+
+app.use((req, res, next) => {
+  console.log("in the middleware_2");
+});
+
 const server = http.createServer(app);
 
 const PORT = process.env.SERVER_PORT;
